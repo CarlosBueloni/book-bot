cdfd03bc5afd5b0f678da2a9541d8ec1afe2eae3
CarlosBueloni <carlosbueloni@gmail.com>
2026-04-08 10:09:24 -0300

    add argument to get book path

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words, count_characters, sort_characters
 
 def get_book_text(filepath):
@@ -5,7 +6,10 @@ def get_book_text(filepath):
         return f.read()
 
 def main():
-    path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path = sys.argv[1]
     text = get_book_text(path)
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {path}...")
